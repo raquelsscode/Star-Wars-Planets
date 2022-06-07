7d65f55fa4e4472dad9db18a1526dc34fa544e1f
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-06-07 18:57:33 -0300

    Adicionando requisito 2

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,43 +1,73 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 function Table() {
-  const { data, getPlanets } = useContext(PlanetsContext);
+  const { data, getPlanets,
+    planetName, setPlanetName } = useContext(PlanetsContext);
+  const [dataFilter, setDataFilter] = useState([]);
 
   useEffect(() => {
     getPlanets();
   }, []);
 
+  useEffect(() => {
+    function FilterPlanetName(planet) {
+      const filterPlanets = data.filter(
+        (item) => item.name.toLowerCase().includes(planet.toLowerCase()),
+      );
+      setDataFilter(filterPlanets);
+    }
+    FilterPlanetName(planetName.filterByName.name);
+  }, [data, planetName.filterByName.name]);
+
+  const handlePlanet = ({ target }) => {
+    setPlanetName(
+      { ...planetName,
+        filterByName: {
+          name: target.value,
+        } },
+    );
+  };
+
   const title = ['Name', 'Rotation Period', 'Orbital Period', 'Diameter',
     'Climate', 'Gravity', 'Terrain', 'Surface Water',
     'Population', 'Films', 'Created', 'Edited', 'URL'];
+
   return (
-    <table>
-      <thead>
-        <tr>
-          {title.map((infos, index) => (<th key={ index }>{infos}</th>))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((planet) => (
-          <tr key={ planet.name }>
-            <td data-testid="planet-name">{ planet.name }</td>
-            <td>{ planet.rotation_period }</td>
-            <td>{ planet.orbital_period }</td>
-            <td>{ planet.diameter }</td>
-            <td>{ planet.climate }</td>
-            <td>{ planet.gravity }</td>
-            <td>{ planet.terrain }</td>
-            <td>{ planet.surface_water }</td>
-            <td>{ planet.population }</td>
-            <td>{ planet.films.map((element) => element) }</td>
-            <td>{ planet.created }</td>
-            <td>{ planet.edited }</td>
-            <td>{ planet.url }</td>
+    <div>
+      <input
+        type="text"
+        placeholder="Procure um Planeta"
+        onChange={ handlePlanet }
+        data-testid="name-filter"
+      />
+      <table>
+        <thead>
+          <tr>
+            {title.map((infos, index) => (<th key={ index }>{infos}</th>))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {dataFilter.map((planet) => (
+            <tr key={ planet.name }>
+              <td data-testid="planet-name">{ planet.name }</td>
+              <td>{ planet.rotation_period }</td>
+              <td>{ planet.orbital_period }</td>
+              <td>{ planet.diameter }</td>
+              <td>{ planet.climate }</td>
+              <td>{ planet.gravity }</td>
+              <td>{ planet.terrain }</td>
+              <td>{ planet.surface_water }</td>
+              <td>{ planet.population }</td>
+              <td>{ planet.films.map((element) => element) }</td>
+              <td>{ planet.created }</td>
+              <td>{ planet.edited }</td>
+              <td>{ planet.url }</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -5,6 +5,14 @@ import PlanetsContext from './PlanetsContext';
 function PlanetsProvider({ children }) {
   const [data, setData] = useState([]);
 
+  const [planetName, setPlanetName] = useState(
+    {
+      filterByName: {
+        name: '',
+      },
+    },
+  );
+
   const getPlanets = async () => {
     const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
     const dataApi = await response.json();
@@ -12,7 +20,7 @@ function PlanetsProvider({ children }) {
   };
 
   const contextData = {
-    data, setData, getPlanets,
+    data, setData, getPlanets, planetName, setPlanetName,
   };
 
   return (
